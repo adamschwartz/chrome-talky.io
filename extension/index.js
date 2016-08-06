02966c1bf3390fa319ccef870613e82e29b32b48
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2016-08-05 20:12:30 -0400

    rotation

--- extension/index.js
+++ extension/index.js
@@ -1,6 +1,7 @@
 (function(window, document, undefined) {
   var scaleX = 1;
   var scaleY = 1;
+  var rotateZ = 0;
 
   var videoChatHasStarted = function() {
     return document.querySelectorAll('video').length > 1;
@@ -12,9 +13,12 @@
     head.appendChild(style);
 
     var setStyle = function() {
-      var transform = 'scaleX(' + scaleX + ') scaleY(' + scaleY + ')';
+      var transform = 'scaleX(' + scaleX + ') scaleY(' + scaleY + ') rotateZ(' + rotateZ + 'deg)';
 
       var styleHTML = '' +
+        '.stream-podium {' +
+        '  background: #000 !important' +
+        '}' +
         '.stream-podium video {' +
         '  -webkit-transform: translate(-50%, -50%) ' + transform + ' !important;' +
         '  transform: translate(-50%, -50%) ' + transform + ' !important;' +
@@ -50,12 +54,22 @@
 
     window.addEventListener('keydown', function(event) {
       if (videoChatHasStarted()) {
-        if (event.keyCode === 37 || event.keyCode === 39) {
-          scaleX *= -1;
-        }
+        if (event.shiftKey || event.ctrlKey) {
+          if (event.keyCode === 37 || event.keyCode === 38) {
+            rotateZ += 45;
+          }
 
-        if (event.keyCode === 38 || event.keyCode === 40) {
-          scaleY *= -1;
+          if (event.keyCode === 39 || event.keyCode === 40) {
+            rotateZ -= 45;
+          }
+        } else {
+          if (event.keyCode === 37 || event.keyCode === 39) {
+            scaleX *= -1;
+          }
+
+          if (event.keyCode === 38 || event.keyCode === 40) {
+            scaleY *= -1;
+          }
         }
       }
 
